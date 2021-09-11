de764fa312e999492c23f032af190e0406852227
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-12 05:24:03 +0600

    signup & login auth with firebase

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,43 @@
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 import Button from "./Button";
 import Form from "./Form";
 import TextInput from "./TextInput";
 
 export default function LoginForm() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
+
+  const { login } = useAuth();
+  const history = useHistory();
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    try {
+      setError("")
+      setLoading(true)
+      await login(email, password,)
+      history.push("/")
+    } catch (err) {
+      setLoading(false)
+      setError("Failed to login!")
+    }
+  }
+
   return (
-    <Form style={{ height: "330px" }} >
+    <Form style={{ height: "330px" }} onSubmit={handleSubmit} >
       <TextInput
         type="text"
         placeholder="Enter email"
         icon="alternate_email"
         required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
 
       <TextInput
@@ -17,15 +45,19 @@ export default function LoginForm() {
         placeholder="Enter password"
         icon="lock"
         required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
 
-      <Button type="submit">
+      <Button type="submit" disabled={loading}>
         <span>Submit Now</span>
       </Button>
 
 
+      {error && <p className={error}>{error}</p>}
+
       <div className="info">
-        Don't have an account? <a href="{#}">Signup</a> instead.
+        Don't have an account? <Link to="signup">Signup</Link> instead.
       </div>
     </Form>
   );
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,25 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import Button from '../components/Button'
 import Checkbox from '../components/Checkbox'
 import Form from '../components/Form'
 import TextInput from '../components/TextInput'
+import { useAuth } from "../contexts/AuthContext"
 import classes from '../styles/Signup.module.css'
 
 const SignupForm = () => {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [agree, setAgree] = useState("")
+
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
+
+  const { signup } = useAuth();
+  const history = useHistory();
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    // ? do validate
+    if (password !== confirmPassword) {
+      return setError("Passwords don't match!")
+    }
+
+    try {
+      setError("")
+      setLoading(true)
+      await signup(email, password, username)
+      history.push("/")
+    } catch (err) {
+      setLoading(false)
+      setError("Failed to create an account!")
+    }
+  }
+
   return (
-
     <>
-      <Form className={classes.signup} >
-        <TextInput type="text" placeholder="Enter name" icon="person" />
-        <TextInput type="email" placeholder="Enter email" icon="alternate_email" />
-        <TextInput type="password" placeholder="Enter password" icon="lock" />
-        <TextInput type="password" placeholder="Confirm password" icon="lock_clock" />
+      <Form className={classes.signup} onSubmit={handleSubmit} >
+        <TextInput
+          type="text"
+          placeholder="Enter name"
+          icon="person"
+          required
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
 
-        <Checkbox text="I agree to the Terms &amp; Conditions" />
-        <Button><span>Submit Now</span></Button>
+        <TextInput
+          type="email"
+          placeholder="Enter email"
+          icon="alternate_email"
+          required value={email}
+          onChange={(email) => setEmail(email.target.value)}
+        />
+
+        <TextInput
+          type="password"
+          placeholder="Enter password"
+          icon="lock"
+          required value={password}
+          onChange={(password) => setPassword(password.target.value)}
+        />
+
+        <TextInput
+          type="password"
+          placeholder="Confirm password"
+          icon="lock_clock"
+          required
+          value={confirmPassword}
+          onChange={(confirmPassword) => setConfirmPassword(confirmPassword.target.value)}
+        />
+
+        <Checkbox
+          text="I agree to the Terms &amp; Conditions"
+          required
+          value={agree}
+          onChange={(agree) => setAgree(agree.target.value)}
+        />
+        <Button type="submit" disabled={loading}>
+          <span>Submit Now</span>
+        </Button>
+
+        {error && <p className={error}>{error}</p>}
 
         <div className="info">
-          Already have an account? <a href="{#}">Login</a> instead.
+          Already have an account? <Link to="/login">Login</Link> instead.
         </div>
       </Form>
     </>
