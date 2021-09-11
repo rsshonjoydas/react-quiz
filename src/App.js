40e3dd625a0c9c4698637fdd540d866ca5bb7236
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-12 03:59:26 +0600

    firebase email and password auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from "./components/Layout";
+import { AuthProvider } from "./contexts/AuthContext";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
@@ -9,15 +10,17 @@ import SignUp from "./pages/SignUp";
 function App() {
   return (
     <Router>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/quiz" component={Quiz} />
-          <Route exact path="/result" component={Result} />
-        </Switch>
-      </Layout>
+      <AuthProvider>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/quiz" component={Quiz} />
+            <Route exact path="/result" component={Result} />
+          </Switch>
+        </Layout>
+      </AuthProvider>
     </Router>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_MESSAGINGSENDERID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
