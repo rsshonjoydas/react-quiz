b83ada07104885179a863adf24cc41495f791f60
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-12 17:17:16 +0600

    show video in ui from firebase database

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,8 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_PROJECTID,
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_MESSAGINGSENDERID
+  appId: process.env.REACT_APP_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL
 };
 
 // Initialize Firebase
